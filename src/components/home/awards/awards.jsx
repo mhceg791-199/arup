7817e2d76fcc1f97cfd3b670bdfa973d789ed8de
MinaNagy99 <minanagykhalefa@gmail.com>
2024-12-23 11:57:13 +0200

    w

--- src/components/home/awards/awards.jsx
+++ src/components/home/awards/awards.jsx
@@ -8,21 +8,75 @@ function Awards() {
   const awardsDetails = [
     {
       img: "/awards/1.webp",
-      title: "OSHEANA CONDOMINIUMS, Edmonton, AB",
-      year: "2006",
-      description: "Won SAM Awards under 2 categories for Suite Design Types",
+      title: "CALLAGHAN RAVINES, Edmonton, AB",
+      year: "2014 - 2015",
+      description: "The Americas Property Awards ,Canada",
     },
     {
       img: "/awards/2.webp",
-      title: "MACKINNON ESTATES, Edmonton, AB",
-      year: "2011",
-      description: "Won the SAM Awards Finalist Town House Category",
+      title: "Lexus, Edmonton, AB",
+      year: "2014 - 2015",
+      description: "The American Property Awards ,Canada ",
     },
     {
       img: "/awards/3.webp",
-      title: "Lexus, Edmonton, AB",
+      title: "MACKINNON ESTATES, Edmonton, AB",
+      year: "2012",
+      description: "The SAM Awards 2011 Finalist Town House Category",
+    },
+    {
+      img: "/awards/4.webp",
+      title: "Mall of America ,Bloomington Minnesota",
       year: "2014 - 2015",
-      description: "Won the American Property Awards Canada",
+      description:
+        "Rated #1 in the list of 10 best malls in world largest malls in USA",
+    },
+    {
+      img: "/awards/5.webp",
+      title: "MULTI-GENERATIONAL HOUSING (M.G.H.), Calgary, AB",
+      year: "2017 - 2018",
+      description: "The Architect Multiple Residence Canada Award ",
+    },
+    {
+      img: "/awards/6.webp",
+      title:
+        "NIGERIAN INDUSTRIAL BANK HEADQUARTERS, CONVENTION CENTRE & SHOPPING COMPLEX, Abuja, Nigeria",
+      year: "2014 - 2015",
+      description: "One of the largest projects in Nigeria.",
+    },
+    {
+      img: "/awards/7.webp",
+      title: "OSHEANA CONDOMINIUMS, Edmonton, AB",
+      year: "2006",
+      description:
+        "SAM Awards 2006 under 2 categories for Suite Design Types Finalist – SAM Awards 2006 as one of the 2 best Apartments.",
+    },
+    {
+      img: "/awards/8.webp",
+      title: "Olympic Center, Calgary, AB",
+      year: "2014 - 2015",
+      description:
+        "Winning design of a limited competition of five local firms. ",
+    },
+    {
+      img: "/awards/9.webp",
+      title: "One Denver Place, Denver, Colorado, USA",
+      year: "2014 - 2015",
+      description:
+        "Won a limited competition among five architectural firms in Calgary",
+    },
+    {
+      img: "/awards/10.webp",
+      title: "WALDEN COMMUNITY – SENIOR’S RESIDENCE",
+      year: "2015 - 2016",
+      description:
+        "The International Property Awards, in the Architecture Residence Canada ",
+    },
+    {
+      img: "/awards/11.webp",
+      title: "West Edmonton Mall , Edmonton, AB",
+      year: "2013 - 2014",
+      description: "Rated #2 in the list of 10 – Best Malls in the World",
     },
   ];
   const settings = {
@@ -63,8 +117,7 @@ function Awards() {
     <>
       <div className="mt-10">
         <div className="ps-3">
-
-        <SectionHeader text="AWARDS" />
+          <SectionHeader text="AWARDS" />
         </div>
         <Slider {...settings}>
           {awardsDetails.map((item, index) => (
