1cd3d63ed8d2fdc9a98675b8ada6e4c781f7be51
MinaNagy99 <minanagykhalefa@gmail.com>
2025-03-10 10:16:19 +0200

    edit some text in about page

--- src/components/about/aboutSection/aboutSection.jsx
+++ src/components/about/aboutSection/aboutSection.jsx
@@ -7,7 +7,8 @@ function AboutSection() {
       <div className="bg-white py-10 text-mainColor md:px-20">
         <SectionHeader text="ABOUT US" />
         <p className="mt-5 text-justify">
-          Arup Datta Architect Ltd. (ADAL), a proud subsidiary of{" "}
+          <span className="font-bold">Arup Datta Architect Ltd.</span> (ADAL), a
+          major subsidiary of{" "}
           <a
             className="font-bold underline"
             target="_blank"
@@ -16,20 +17,20 @@ function AboutSection() {
             Mosaic Holding Corporation
           </a>
           , is a globally recognized multidisciplinary firm with over 40 years
-          of expertise in delivering exceptional design solutions. Comprising a
+          of expertise in delivering exceptional design solutions for
+          architecture, Master Planning, Urban and Interior Design. Comprising a
           skilled team of architects, architectural technologists, project
           managers, interior designers, and dedicated support professionals,
           ADAL has established a stellar reputation for excellence across
-          residential, commercial, and institutional sectors. The firm
-          specializes in master and urban planning, interior design, and a wide
-          range of architectural projects, including single-family and
-          multifamily residential developments, commercial and administrative
-          spaces, healthcare and cultural buildings, and creative endeavors such
-          as theme parks, set designs, and water features. Additionally, ADAL
-          provides comprehensive regulatory and technical services, including
-          building booklets and permit facilitation. Committed to innovation,
-          quality, and functionality, ADAL continues to shape visionary and
-          enduring spaces worldwide.
+          Residential, Commercial, Mixed-use and Institutional sectors. The firm
+          specializes in a wide range of architectural categories which includes
+          major Commercial, Mixed-Use, Multi Residential, Senior Housing and
+          Institutional Building creating creative endeavors such as theme
+          parks, set designs, and water features. Additionally, ADAL provides
+          comprehensive regulatory and technical services, including building
+          booklets and permit facilitation. Committed to innovation, quality,
+          and functionality, ADAL continues to shape visionary and enduring
+          spaces worldwide.
         </p>
       </div>
       <div
@@ -44,7 +45,7 @@ function AboutSection() {
           project achieves both budgetary alignment and aesthetic distinction.
           Since its establishment, ADAL has consistently delivered innovative,
           contextually sensitive designs that prioritise functionality,
-          efficiency, and user-centricity. The firm is dedicated to crafting
+          efficiency, and user-friendly. The firm is dedicated to crafting
           architecture that harmonises with its environment while addressing the
           distinct needs of clients and end users, resulting in dynamic spaces
           that stand the test of time.{" "}
@@ -55,14 +56,15 @@ function AboutSection() {
           Middle East, and Southeast Asia. Its projects are celebrated for their
           exceptional quality, cost-efficiency, and profound community impact.
           Arup Datta’s commitment to societal betterment extends beyond design,
-          with active involvement in organisations such as the Chamber of
-          Commerce, the Canadian Cancer Society.{" "}
+          with active involvement in organisations such as Canadian Cancer
+          Society, Royal Architecture Institute of Canada, and Chamber of
+          Commerce
         </p>
         <p className="mt-5 text-justify">
-          Registered in Alberta, British Columbia, Ontario, and Saskatchewan,
-          ADAL also undertakes projects in the USA, India, Malaysia, Qatar,
-          Saudi Arabia, and Oman, continuing its legacy of transforming global
-          landscapes through thoughtful and visionary design.
+          Registered in Alberta, British Columbia, Ontario, Saskatchewan and
+          Washington, ADAL also undertakes projects in the USA, India, Malaysia,
+          Qatar, Saudi Arabia, and Oman, continuing its legacy of transforming
+          global landscapes through thoughtful and visionary design.
         </p>
       </div>
     </>
