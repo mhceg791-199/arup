faaeda0671153cc7ca8e9847616e838d26445d0c
minanagy99 <minanagykhalefa@gmail.com>
2025-06-30 19:30:50 +0300

    change direction for build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "../../var/www/arup", // Replace with your desired directory path
+  },
   plugins: [react()],
 })
