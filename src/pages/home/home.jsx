f5dcfd2a43be534e5554693eb04d58376d2a288a
mhceg791-199 <mhceg791@gmail.com>
2025-10-06 18:16:03 +0300

    updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,11 +4,13 @@ import Numbers from "../../components/home/numbers/numbers";
 import Awards from "../../components/home/awards/awards";
 import JoinUs from "../../components/home/joinUs/joinUs";
 import VideoPoster from "../../components/shared/videoPoster/videoPoster";
+import ImagePoster from "../../components/shared/heroImage/heroImage";
 
 function Home() {
   return (
     <>
-      <VideoPoster video="/home/home.mp4" />
+      {/* <VideoPoster video="/home/home.mp4" /> */}
+      <ImagePoster image="/home/home.webp" />
       <div className="px-7">
         <Numbers />
         <Services />
