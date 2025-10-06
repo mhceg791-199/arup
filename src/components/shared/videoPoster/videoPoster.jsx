e41e5bfddc1a6f891b559142da947658a209d70f
mhceg791-199 <mhceg791@gmail.com>
2025-10-06 17:48:16 +0300

    updated

--- src/components/shared/videoPoster/videoPoster.jsx
+++ src/components/shared/videoPoster/videoPoster.jsx
@@ -2,33 +2,16 @@ import React from "react";
 
 function VideoPoster({ video }) {
   return (
-    <img
-      className="w-full h-[80vh] object-cover"
-      src={video}
-      alt="Poster"
-    />
+    <>
+      <video
+        className="w-full h-[80vh] object-cover"
+        autoPlay
+        muted
+        loop
+        src={video}
+      ></video>
+    </>
   );
 }
 
 export default VideoPoster;
-
-
-
-
-// import React from "react";
-
-// function VideoPoster({ video }) {
-//   return (
-//     <>
-//       <video
-//         className="w-full h-[80vh] object-cover"
-//         autoPlay
-//         muted
-//         loop
-//         src={video}
-//       ></video>
-//     </>
-//   );
-// }
-
-// export default VideoPoster;
