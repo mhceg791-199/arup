90c9a77717163cde13b12a905f6dd29e43d584bd
mhceg791-199 <mhceg791@gmail.com>
2025-10-06 16:41:31 +0300

    updated

--- src/components/shared/clientCard/clientCard.jsx
+++ src/components/shared/clientCard/clientCard.jsx
@@ -3,15 +3,17 @@ import React from "react";
 function ClientCard({ client }) {
   const { title, img, text } = client;
   return (
-    <>
-      <div className="bg-mainColor p-5">
-        <div className="h-[6rem]">
-<img className="mb-3 object-contain" src={img} alt="" />
-        </div>
-        <p className="text-gold my-1 font-bold">{title}</p>
-        <p>{text}</p>
+    <div className="bg-mainColor p-5">
+      <div className="h-[6rem] w-full overflow-hidden flex items-center justify-center">
+        <img
+          className="object-contain w-full h-full rounded-md"
+          src={img}
+          alt={title}
+        />
       </div>
-    </>
+      <p className="text-gold my-2 font-bold">{title}</p>
+      <p className="text-white text-sm">{text}</p>
+    </div>
   );
 }
 
